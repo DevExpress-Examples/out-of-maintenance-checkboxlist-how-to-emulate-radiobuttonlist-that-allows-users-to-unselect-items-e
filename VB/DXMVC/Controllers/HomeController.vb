7c8ff96f09ff6daf56e01e57c54c6d8b89efc8bd
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/DXMVC/Controllers/HomeController.vb
@@ -0,0 +1,19 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.Mvc
+
+Namespace DXMVC.Controllers
+	Public Class HomeController
+		Inherits Controller
+		'
+		' GET: /Home/
+
+		Public Function Index() As ActionResult
+			Return View()
+		End Function
+
+	End Class
+End Namespace
---
+++ VB/DXMVC/Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Codebehind="Global.asax.vb" Inherits="DXMVC.MvcApplication" Language="vb" %>
